712c9717fbea7db175de811b468d3c699ed091fc
thomasbatt <thomasbatt@gmail.com>
2016-06-21 20:03:15 +0200

    initialisation

---
+++ config/dbConfigSample.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * ICI on met les information de la base de données
+ * le port 3306 est le port par défaut de mysql sous Linux
+ * Si vous êtes sous mamp il me semble qu'il faut mettre 8000 ou un truc dans le genre
+ * Sous la chose, aucune idée et je ne veut même pas le savoir ! LOL !
+ */
+$config = [
+  'dbPort' => '3306',
+  'dbName' => 'YourDatabase',
+  'dbHost' => 'localhost',
+  'dbUser' => 'YourUser',
+  'dbPassword' => 'YourPassword'
+];
+
+?>
