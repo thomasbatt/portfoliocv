5a8d8147d26aff0b63b237d22b93bda7a1e891bb
thomasbatt <thomasbatt@gmail.com>
2016-07-22 21:54:20 +0200

    sass add @import

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var $ = require('gulp-load-plugins')();
 var cleanCSS = require('gulp-clean-css');
 
 var sassPaths = [
-    'bower_components/bootstrap-sass/stylesheets',
+    'bower_components/bootstrap-sass/assets/stylesheets',
     'bower_components/wow/css/libs',
     'bower_components/devicons/css',
-    'bower_components/fontawesome/scss'
+    'bower_components/font-awesome/scss'
 ];
 
 gulp.task('sass', function() {
@@ -21,18 +21,18 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('assets/css'));
 });
 
-gulp.task('minify-css', function() {
-    return gulp.src('assets/css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('assets/css'));
-});
+// gulp.task('minify-css', function() {
+//     return gulp.src('assets/css/*.css')
+//     .pipe(cleanCSS({compatibility: 'ie8'}))
+//     .pipe(gulp.dest('assets/css'));
+// });
 
 gulp.task('icons', function() { 
     return gulp.src('bower_components/fontawesome/fonts/**.*') 
         .pipe(gulp.dest('./assets/fonts')); 
 });
 
-gulp.task('default', ['sass', 'minify-css', 'icons'], function() {
+gulp.task('default', ['sass', 'icons'], function() {
     gulp.watch(['scss/**/*.scss'], ['sass']);
     // gulp.watch(['assets/css/*.css'], ['minify-css']);
 });
