21f7e2a7428029b412379d9a422f470c9ccba69b
thomasbatt <thomasbatt@gmail.com>
2016-08-08 00:50:24 +0200

    update bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,72 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
-var cleanCSS = require('gulp-clean-css');
+var bower = require('./bower');
 
-var concat = require('gulp-concat');  
-var rename = require('gulp-rename');  
-var uglify = require('gulp-uglify'); 
-var lazypipe = require('lazypipe');
-
-var bower = require('./bower.json');
-var bo = 'bower_components/'; 
-
-
+function bowerPath(url){
+    for (i = 0; i < url.length; i++) {
+      url[i] = bower.main['bowerPath']+"/"+url[i];
+    }
+    return url;
+}
 // -----------------------CSS ASSETS---------------------------
-// var sassPaths = [
-//     bo+'bootstrap-sass/assets/stylesheets',
-//     bo+'animate.css',
-//     bo+'devicons/css',
-//     bo+'devicon',
-//     bo+'elh-tooltip',
-//     bo+'font-awesome/scss'
-// ];
-gulp.task('sass', function() {
-    gulp.src('scss/app.scss')
-        .pipe(plugins.sass({
-            includePaths: bower.sassPaths
-        })
-        .on('error', plugins.sass.logError))
-        .pipe(plugins.autoprefixer({
-            browsers: ['last 20 versions', 'ie >= 9']
-        }))
-        // .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(rename("website.css"))
-        .pipe(gulp.dest('assets/css'));
+var bowerSassPaths = bowerPath(bower.sassPaths);
 
-    gulp.src('scss/vendors/vendors.scss')
+gulp.task('sass', function() {
+    gulp.src(bower.main['srcPath']+'scss/app.scss')
         .pipe(plugins.sass({
-            includePaths: bower.sassPaths
+            includePaths: bowerSassPaths
         })
         .on('error', plugins.sass.logError))
         .pipe(plugins.autoprefixer({
             browsers: ['last 20 versions', 'ie >= 9']
         }))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(rename("vendors.min.css"))
-        .pipe(gulp.dest('assets/css'));
+        .pipe(plugins.cleanCss({compatibility: 'ie8'}))
+        .pipe(plugins.rename("website.css"))
+        .pipe(gulp.dest(bower.main['destPath']+'/css'));
+
+    gulp.src(bower.main['srcPath']+'scss/vendors/vendors.scss')
+        .pipe(plugins.sass({
+            includePaths: bowerSassPaths
+        })
+        .on('error', plugins.sass.logError))
+        .pipe(plugins.autoprefixer({
+            browsers: ['last 20 versions', 'ie >= 9']
+        }))
+        .pipe(plugins.cleanCss({compatibility: 'ie8'}))
+        .pipe(plugins.rename(bower.main['destFile']+".min.css"))
+        .pipe(gulp.dest(bower.main['destPath']+'/css'));
 });
 
 
 // -----------------------JS ASSETS---------------------------
-// var jsVendorsFiles = [
-//     bo+'angular/angular.min.js',
-//     bo+'angular-parallax/scripts/angular-parallax.js',
-//     bo+'angular-scroll/angular-scroll.min.js',
-//     bo+'jquery/dist/jquery.min.js',
-//     bo+'ngSticky/dist/sticky.min.js',
-//     bo+'wow/dist/wow.min.js',
-//     'scripts/vendors/tooltip.min.js',
-// ];
-var jsWebsiteFiles = [
-    'scripts/interface/**.js',
-    'scripts/**.js',
-];
+
 gulp.task('scripts', function() { 
-    gulp.src(jsWebsiteFiles) 
-        .pipe(concat('website.js'))
-        .pipe(gulp.dest('assets/js')); 
-    gulp.src(bower.jsVendorsFiles)
-        .pipe(concat('vendors.min.js'))
-        // .pipe(uglify())
-        .pipe(gulp.dest('assets/js'));
+    gulp.src([
+            bower.main['srcPath']+'scripts/interface/**.js',
+            bower.main['srcPath']+'scripts/**.js',
+            bower.main['srcPath']+'scripts/vendors/tooltip.min.js'
+        ]) 
+        .pipe(plugins.concat('website.js'))
+        // .pipe(plugins.uglify())
+        .pipe(gulp.dest(bower.main['destPath']+'/js')); 
+    gulp.src(bowerPath(bower.jsVendorsFiles))
+        .pipe(plugins.concat(bower.main['destFile']+'.min.js'))
+        // .pipe(plugins.uglify())
+        .pipe(gulp.dest(bower.main['destPath']+'/js'));
 });
 
 
 // -----------------------FONTS ASSETS---------------------------
-var iconsFiles = [
-    bo+'bootstrap-sass/assets/fonts/*/**.*',
-    bo+'devicons/fonts/**.*',
-    bo+'devicon/fonts/**.*',
-    bo+'font-awesome/fonts/**.*'
-];
 gulp.task('icons', function() { 
-    return gulp.src(iconsFiles) 
-        .pipe(gulp.dest('assets/fonts')); 
+    return gulp.src(bowerPath(bower.iconsFiles)) 
+        .pipe(gulp.dest(bower.main['destPath']+'/fonts')); 
 });
 
 
 // -------------------------WATCHERS----------------------------
 gulp.task('default', ['sass','icons','scripts'], function() {
-    gulp.watch(['scss/**/**.scss'], ['sass']);
-    gulp.watch(['scripts/**/**.js'], ['scripts']);
+    gulp.watch([bower.main['srcPath']+'scss/**/**.scss'], ['sass']);
+    gulp.watch([bower.main['srcPath']+'scripts/**/**.js'], ['scripts']);
 });
 
 
